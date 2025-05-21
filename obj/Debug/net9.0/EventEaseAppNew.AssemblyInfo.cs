689b2d2b505ba9690c4e7450398750193e76d7de
mytzike <daisy.leming>
2025-05-21 17:12:48 +0800

    changes

--- obj/Debug/net9.0/EventEaseAppNew.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEaseAppNew.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseAppNew")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a3d2a615e8321e3f7fb4b4ff1875e0d7777868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f52c578e881e130e611071f9dc9c59855eaafa")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseAppNew")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseAppNew")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
